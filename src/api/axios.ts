70217ab9177af5adddec3568cd9d23a20190b39d
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-01-13 20:23:27 +0900

    API 불러오기

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import type { MessageResponse } from '../types/todo';
 
 export const api = axios.create({
     baseURL: import.meta.env.API_KAY,
@@ -7,3 +8,48 @@ export const api = axios.create({
     Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
     },
 })
+
+export const getTodo = async() => { //괄호안에 들어가는 것은 만약 api에 매개변수가 필요하면 저 자리에 필요한 매개변수를 적는 공간이다.
+    try{
+        const {data} = await api.get<Response[]>("/posts"); //꺽쇠안에는 있는것은 타입을 말하는데 API 응답으로 Response 배열 안에 있는 타입으로 들어올것이라고 알려주는것
+        return data
+    } catch {
+        console.error('error');
+    }
+}
+
+export const addTodo = async(body : Request) => {
+    try{
+        const {data} = await api.post<MessageResponse>('/posts',body);
+        return data
+    } catch{
+        console.error('error')
+    }
+}
+
+export const authorTodo = async(author : string) => {
+    try {
+        const {data} = await api.get<Response[]>('/posts/author',{params : {author : author}});
+        return data
+    } catch {
+        console.error('error')
+    }
+}
+
+export const putTodo = async(id : number , body : Request) => {
+    try{
+        const {data} = await api.put<MessageResponse>(`/posts/${id}`,body)
+        return data
+    } catch {
+        console.error('error')
+    }
+}
+
+export const DeleteTodo = async(id : number) => {
+    try{
+        const {data} = await api.delete<MessageResponse>(`/posts/${id}`)
+        return data
+    } catch {
+        console.error('error')
+    }
+}
